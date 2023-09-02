e70ff55ace388968bba99b9c5a046337eceb5250
Cosmicflamethrower <43681059+Cosmicflamethrower@users.noreply.github.com>
2023-09-02 23:09:46 +0530

    Added footer.js
    Footer.js is a JavaScript script that adds a generic footer to all HTML documents that cite it

---
+++ footer.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var footer = document.createElement("div");
+    footer.innerHTML = "<p>Email: antimatter4096@gmail.com</p>";
+    footer.id = "footer";
+    var body = document.body;
+    body.appendChild(footer)
+});
